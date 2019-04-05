3b295f771fb6f58c47cf88bf07f0105026451890
Michalina Andrzejewska <andrmichgithub@mismail.xyz>
2019-04-05 15:14:18 +0200

    Files management

---
+++ python_fundamentals/files_management/files_managment.py
@@ -0,0 +1,22 @@
+f = open('wasteland.txt', mode='wt', encoding='utf-8')
+
+f. write('What are the roots that clutch, ')
+f.write('what branches grow\n')
+f. close()
+
+g = open('wasteland.txt', mode='rt', encoding='utf-8'))
+g.read(32)
+g.read()
+
+g.seek(0)
+g.readline()
+g.readlines()
+
+h = open('wasteland.txt', mode='at', encoding='utf-8'))
+
+# h.writelines(
+#         ['Son of man, \n',
+#         'You cannot say, or guess, ',
+#         'for you know only, \n',
+#         'A heap of broken images, ',
+#         'where the sun beats\n'])
